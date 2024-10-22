161df35f6e9ab67b2fc58230d14184c2c31d0fd4
PabloMagn0 <pablojaviereg@gmail.com>
2024-10-21 22:05:25 -0500

    cambios en imports

--- ArriendaTuFincaFront/src/app/app.routes.ts
+++ ArriendaTuFincaFront/src/app/app.routes.ts
@@ -5,16 +5,12 @@ import { RegistroComponent } from './components/registro/registro.component';
 import { BuscarComponent } from './components/buscar/buscar.component';
 import { MisPropiedadesComponent } from './components/mispropiedades/mispropiedades.component';
 import { AlquilerListaComponent } from './components/alquiler-lista/alquiler-lista.component';
-import { BuscarComponent } from './components/buscar/buscar.component';
 import { EditarPerfilComponent } from './components/editar-perfil/editar-perfil.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
 import { MiPerfilComponent } from './components/mi-perfil/mi-perfil.component';
-import { MisPropiedadesComponent } from './components/mispropiedades/mispropiedades.component';
 import { SolicitarArriendoComponent } from './components/solicitar-arriendo/solicitar-arriendo.component';
-import { RegistroComponent } from './registro/registro.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' }, 
